782992b7551caa093c89c3d8b53c308cf6731332
hnemmass <hnemmass>
2025-06-18 14:04:49 +0100

    test

--- inc/execution.h
+++ inc/execution.h
@@ -6,7 +6,7 @@
 /*   By: hnemmass <hnemmass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 17:11:28 by hnemmass          #+#    #+#             */
-/*   Updated: 2025/06/11 16:01:56 by hnemmass         ###   ########.fr       */
+/*   Updated: 2025/06/17 16:06:06 by hnemmass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ char	*get_next_line(int fd);
 char	**ft_split_2(char const *s, char c);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strdup(char *s);
+char	*ft_strndup(const char *s, int n);
 int		apply_redirections(t_redirect *red, t_minishell *mini);
 int		ft_cd(char **cmd, t_env *env, t_minishell *mini);
 int		ft_echo(char **cmd);
